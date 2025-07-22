165a5ca4801552de00864843b839710f2a01f39c
devurmil <urmilramani124@gmail.com>
2025-07-22 23:29:18 +0530

    Profile Picture

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
 interface User {
+  profile_picture: string;
   id: string;
   name: string;
   email: string;
